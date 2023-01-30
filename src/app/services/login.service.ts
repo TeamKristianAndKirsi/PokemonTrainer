21117f3fc0933f0487561b7c00746986461d9426
KipaTa <kirsi.tainio@gmail.com>
2023-01-30 14:36:04 +0200

    first commit

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor() { }
+
+  // Login
+
+  // Check if user exists
+
+  // IF NOT user - Create a user
+
+  // IF user || Created user - Store user
+}
