d409d4b336952445259d3a04d55c6e644eea739f
KipaTa <kirsi.tainio@gmail.com>
2023-01-31 14:34:48 +0200

    Fix: Environment issues & api key removal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 
-// environmetn import herjaa että ei ole moduli, tarvitaanko edes? 
-//import { environment } from 'src/environments/environments';
-
 
 @Component({
   selector: 'app-root',
--- src/environments/environments.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,
-    apiUsers: "https://quiver-distinct-shadow.glitch.me/trainers",
-    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=1300&offset=0"
-};
