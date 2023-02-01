57526b2e9be9d8715fa0df3697d67f4bcc01ffe7
KipaTa <kirsi.tainio@gmail.com>
2023-02-01 10:03:01 +0200

    Feat: Login user service redirect

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.css']
 })
-export class LoginPage {
+export class LoginPage{
+
+  constructor(private readonly router: Router) {}
+
+ 
+
+  handleLogin(): void {
+    this.router.navigateByUrl("/pokemons");
+  }
 
 }
