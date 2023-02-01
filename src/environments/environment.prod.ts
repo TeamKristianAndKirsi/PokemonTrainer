10a8d65ee2094f4cdea332dcaadf2232edb16e92
KipaTa <kirsi.tainio@gmail.com>
2023-02-01 14:25:40 +0200

    Fix: ImgUrl fix again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUsers: "https://quiver-distinct-shadow.glitch.me/trainers",
-    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=1300&offset=0"
+    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
 };
