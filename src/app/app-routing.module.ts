137644380abbec984dd6b71154503c5c33d08d71
Kristian Wink <kristian.wink@fi.experis.com>
2023-02-03 13:52:57 +0200

    Feat: Final styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
         path: "pokemons",
         component: PokemonCataloguePage,
         canActivate: [ AuthGuard ]
+        
     },
     {
         path: "trainer",
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
+    pokemonApiKey: "6f4a8118-7722-45bc-9895-e6d70f0eb3a6",
     apiUsers: "https://quiver-distinct-shadow.glitch.me/trainers",
-    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0"
+    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
+    imgURL: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
 };
