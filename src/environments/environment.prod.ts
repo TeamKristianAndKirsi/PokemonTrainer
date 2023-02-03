09f1cd9e49eecda348c7f73fa0cad64d454aaa9c
Kristian Wink <kristian.wink@fi.experis.com>
2023-02-03 14:06:05 +0200

    Fix: Limit size changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
-    production: true,
-    pokemonApiKey: "6f4a8118-7722-45bc-9895-e6d70f0eb3a6",
-    apiUsers: "https://quiver-distinct-shadow.glitch.me/trainers",
-    apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",
-    imgURL: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+  production: true,
+  pokemonApiKey: '6f4a8118-7722-45bc-9895-e6d70f0eb3a6',
+  apiUsers: 'https://quiver-distinct-shadow.glitch.me/trainers',
+  apiPokemons: 'https://pokeapi.co/api/v2/pokemon?limit=200&offset=0',
+  imgURL:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
 };
